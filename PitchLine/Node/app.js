c76c97697aae5fa8a08e46563f967d39f75afcf5
Yatish20162 <88579423+Yatish20162@users.noreply.github.com>
2022-05-01 18:56:35 +0530

    fifth commit

--- PitchLine/Node/app.js
+++ PitchLine/Node/app.js
@@ -14,11 +14,49 @@ app.listen(8000,()=>{
     console.log("server started");
 })
 
+// database connection
+// -----------------------------------------------
+var dbconfig={
+    host:"localhost",
+    user:"root",
+    password:"yatish@2002",
+    database:"pitchline"
+}
+
+var dbCtrl=mysql.createConnection(dbconfig);
+
+dbCtrl.connect(function(err){
+    if(err)
+    console.log(err);
+    else
+    console.log("connection established");
+})
+// ----------------------------------------------------
+
 
 
 app.get("/",function(req,resp){
-    // console.log(process.cwd());
     resp.sendFile(process.cwd() + "/public/HTML/index.html");
 })
 
+
+app.get("/ajaxCheckUser",function(req,resp){
+    console.log(req.query.mail);
+    dbCtrl.query("select * from USERS where email=?",req.query.mail,function(err,result){
+        console.log(result);
+        if(err)
+            resp.send(err);
+            else
+            {
+              if(result.length==0)
+                resp.send("Available");
+                else
+                resp.send("Already Occupied");
+            }
+    })
+})
+
+app.get("/Signupajax",function(req,resp){
+
+})
 
