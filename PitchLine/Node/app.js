84aae57169e0a01db5b460bfb04bd599ce1bc847
Yatish20162 <88579423+Yatish20162@users.noreply.github.com>
2022-05-01 13:30:58 +0530

    third commit

---
+++ PitchLine/Node/app.js
@@ -0,0 +1,23 @@
+var express=require("express");
+var fileuploader=require("express-fileupload");
+var mysql=require("mysql");
+const { urlencoded } = require("express");
+
+
+var app=express();
+
+// this line is added to add js and css to the file 
+app.use(express.static("public"));
+
+
+app.listen(8000,()=>{
+    console.log("server started");
+})
+
+
+app.get("/",function(req,resp){
+    // console.log(process.cwd());
+    resp.sendFile(process.cwd() + "/public/HTML/index.html");
+})
+
+
